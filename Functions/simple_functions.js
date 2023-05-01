45fbcb18942f6a208ff6a721d086852e0cbd5688
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-05-01 11:25:01 -0400

    created simple_functions.js file

---
+++ Functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings() {
+    console.log("Ariel Kelbore");
+    console.log("Lucy");
+    console.log("Dawit Getachew");
+}
+
+function whyImHere() {
+    console.log("I joined this program because i want to get into the technology industry");
+}
+
+function favoritePlace() {
+    console.log("I want to visit Israel because i want to see all the ancient places");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
