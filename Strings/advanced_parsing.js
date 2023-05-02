2bafc00ee21d9471f75f729772a3f5fa3386e35c
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-05-02 12:51:16 -0400

    modified advanced_parsing.js file

--- Strings/advanced_parsing.js
+++ Strings/advanced_parsing.js
@@ -1,24 +1,25 @@
 function parseAndDisplayName(name) {
 
-    if(name.indexOf(" ") == null) {
+    if(name.indexOf(" ") == -1) {
         console.log(`Name: ${name}`);
         console.log(`Only name: ${name.substring(0, name.length)}`);
     }
-    else if(name.indexOf(" ") != null) {
+    else if(name.indexOf(" ") > -1) {
         console.log(`Name: ${name}`);
         console.log(`First name: ${name.substring(0, name.indexOf(" "))}`);
         console.log(`Last name: ${name.substring(name.indexOf(" ") + 1)}`);
     }
-    else if (name.lastIndexOf(" ") != null) {
+    else if ((name.lastIndexOf(" ") - name.length) < 0) {
         console.log(`Name: ${name}`);
         console.log(`First name: ${name.substring(0, name.indexOf(" "))}`);
-        let firstIndexOfMiddleName = name.substring(name.indexOf(" ")) + 1;
-        console.log(`Middle name: ${name.substring(firstIndexOfMiddleName, name.lastIndexOf(" "))}`);
-        let firstIndexOfLastName = name.substring(name.lastIndexOf(" ")) + 1;
-        console.log(`Last name: ${name.substring(firstIndexOfLastName)}`);
+        console.log(`Middle name: ${name.substring(name.indexOf(" "), name.lastIndexOf(" "))}`);
+        console.log(`Last name: ${name.substring(name.lastIndexOf(" ")+1, name.length)}`);
     }
 }
 
 parseAndDisplayName("Cher");
 parseAndDisplayName("Brenda Kaye");
 parseAndDisplayName("Dana Lynn Wyatt");
+
+
+
