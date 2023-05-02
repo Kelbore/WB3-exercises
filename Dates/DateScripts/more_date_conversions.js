baf7ad18bbe2c8d50ba3c41d8ecc418cf4fbc463
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-05-02 15:58:11 -0400

    created three js files

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,7 @@
+let birthDay = new Date("1994-03-18T00:00:00-0400");
+let momBirthDay = new Date("2023-02-05T00:00:00-0500");
+let dadBirthDay = new Date("2023-02-05T00:00:00-0500");
+
+console.log(birthDay.toLocaleString());
+console.log(momBirthDay.toLocaleString());
+console.log(dadBirthDay.toLocaleString());
