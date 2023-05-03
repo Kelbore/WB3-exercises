36c0b12546b11828a8362b88957577060300392e
Ariel Kelbore <akelbore@appdev.yearup.org>
2023-05-03 09:30:40 -0400

    modified custom_date_format.js file

--- Dates/DateScripts/custom_date_format.js
+++ Dates/DateScripts/custom_date_format.js
@@ -1,11 +1,14 @@
+const d = new Date();
+
 const months = ["January", "February", "March", "April",
                 "May","June","July","August","September", "October",
                 "November","December"];
-const d = new Date();
+
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday"];
 
 let day = d.getDate();
 let month = months[d.getMonth()];
 let year = d.getFullYear();
-let weekDayName = d.getDay();
+let weekDayName = weekdays[d.getDay()];
 
 console.log(`${day}-${month}-${year}-${weekDayName}`);
